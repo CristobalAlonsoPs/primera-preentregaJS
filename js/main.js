1abf5f11f3b5707e666635489c00fcc7b6c1f571
Cristobal Perez <cr.prez18@gmail.com>
2024-04-23 01:53:49 -0300

    Primera Pre entrega corregida

--- js/main.js
+++ js/main.js
@@ -1,64 +1,68 @@
 function sumar(a, b) {
-    return a + b;
-  }
-  
-  function restar(a, b) {
-    return a - b;
-  }
-  
-  function multiplicar(a, b) {
-    return a * b;
-  }
-  
-  function dividir(a, b) {
-    if (b !== 0) {
+  return a + b;
+}
+
+function restar(a, b) {
+  return a - b;
+}
+
+function multiplicar(a, b) {
+  return a * b;
+}
+
+function dividir(a, b) {
+  if (b !== 0) {
       return a / b;
-    } else {
+  } else {
       return "Division por 0 es indefinido";
-    }
   }
-  
-  function obtenerOperacion() {
-    const operacion = parseInt(prompt("Seleccione una operación:\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Salir"));
-    
-    if (operacion < 1 || operacion > 5 || isNaN(operacion)) {
+}
+
+function obtenerOperacion() {
+  const operacion = parseInt(prompt("Seleccione una operación:\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Salir"));
+
+  if (operacion < 1 || operacion > 5 || isNaN(operacion)) {
       alert("Opción inválida. Por favor, seleccione un número del 1 al 5.");
       return obtenerOperacion();
-    } else {
+  } else {
       return operacion;
-    }
   }
+}
+
+function calcular() {
+  let continuar = true;
   
-  function calcular() {
-    const operacion = obtenerOperacion();
-    
-    if (operacion === 5) {
-      alert("Gracias por utilizar la calculadora 2.0");
-      return;
-    }
-  
-    let num1 = parseInt(prompt("Ingresa el primer número"));
-    let num2 = parseInt(prompt("Ingresa el segundo número"));
-  
-    let resultado;
-  
-    switch (operacion) {
-      case 1:
-        resultado = sumar(num1, num2);
-        break;
-      case 2:
-        resultado = restar(num1, num2);
-        break;
-      case 3:
-        resultado = multiplicar(num1, num2);
-        break;
-      case 4:
-        resultado = dividir(num1, num2);
-        break;
-      default:
-        resultado = "Operación no válida";
-    }
-    alert("El resultado es: " + resultado);
-    calcular();
-  }
-  calcular();
+  do {
+      const operacion = obtenerOperacion();
+      
+      if (operacion === 5) {
+          alert("Gracias por utilizar la calculadora 2.0");
+          continuar = false;
+      } else {
+          let num1 = parseInt(prompt("Ingresa el primer número"));
+          let num2 = parseInt(prompt("Ingresa el segundo número"));
+      
+          let resultado;
+      
+          switch (operacion) {
+              case 1:
+                  resultado = sumar(num1, num2);
+                  break;
+              case 2:
+                  resultado = restar(num1, num2);
+                  break;
+              case 3:
+                  resultado = multiplicar(num1, num2);
+                  break;
+              case 4:
+                  resultado = dividir(num1, num2);
+                  break;
+              default:
+                  resultado = "Operación no válida";
+          }
+          alert("El resultado es: " + resultado);
+      }
+  } while (continuar);
+}
+
+calcular();

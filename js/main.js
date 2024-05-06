abae1c962a6cdabb6848955d163339b26e46dcd3
Cristobal Perez <cr.prez18@gmail.com>
2024-05-06 15:15:57 -0400

    Segunda Pre entrega

--- js/main.js
+++ js/main.js
@@ -1,68 +1,67 @@
-function sumar(a, b) {
-  return a + b;
-}
-
-function restar(a, b) {
-  return a - b;
-}
-
-function multiplicar(a, b) {
-  return a * b;
-}
-
-function dividir(a, b) {
-  if (b !== 0) {
+// Objeto 
+const calculadora = {
+  sumar: function(a, b) {
+    return a + b;
+  },
+  restar: function(a, b) {
+    return a - b;
+  },
+  multiplicar: function(a, b) {
+    return a * b;
+  },
+  dividir: function(a, b) {
+    if (b !== 0) {
       return a / b;
-  } else {
-      return "Division por 0 es indefinido";
-  }
-}
+    } else {
+      return "División por 0 es indefinida";
+    }
+  },
 
-function obtenerOperacion() {
-  const operacion = parseInt(prompt("Seleccione una operación:\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Salir"));
+  obtenerOperacion: function() {
+    const operacion = parseInt(prompt("Seleccione una operación:\n1. Sumar\n2. Restar\n3. Multiplicar\n4. Dividir\n5. Salir"));
 
-  if (operacion < 1 || operacion > 5 || isNaN(operacion)) {
+    if (operacion < 1 || operacion > 5 || isNaN(operacion)) {
       alert("Opción inválida. Por favor, seleccione un número del 1 al 5.");
-      return obtenerOperacion();
-  } else {
+      return this.obtenerOperacion(); // Utilizamos 'this' para referirnos al objeto calculadora
+    } else {
       return operacion;
-  }
-}
+    }
+  },
+  calcular: function() {
+    let continuar = true;
 
-function calcular() {
-  let continuar = true;
-  
-  do {
-      const operacion = obtenerOperacion();
-      
+    do {
+      const operacion = this.obtenerOperacion();
+
       if (operacion === 5) {
-          alert("Gracias por utilizar la calculadora 2.0");
-          continuar = false;
+        alert("Gracias por utilizar la calculadora 2.0");
+        continuar = false;
       } else {
-          let num1 = parseInt(prompt("Ingresa el primer número"));
-          let num2 = parseInt(prompt("Ingresa el segundo número"));
-      
-          let resultado;
-      
-          switch (operacion) {
-              case 1:
-                  resultado = sumar(num1, num2);
-                  break;
-              case 2:
-                  resultado = restar(num1, num2);
-                  break;
-              case 3:
-                  resultado = multiplicar(num1, num2);
-                  break;
-              case 4:
-                  resultado = dividir(num1, num2);
-                  break;
-              default:
-                  resultado = "Operación no válida";
-          }
-          alert("El resultado es: " + resultado);
-      }
-  } while (continuar);
-}
+        let num1 = parseInt(prompt("Ingresa el primer número"));
+        let num2 = parseInt(prompt("Ingresa el segundo número"));
 
-calcular();
+        let resultado;
+
+        switch (operacion) {
+          case 1:
+            resultado = this.sumar(num1, num2);
+            break;
+          case 2:
+            resultado = this.restar(num1, num2);
+            break;
+          case 3:
+            resultado = this.multiplicar(num1, num2);
+            break;
+          case 4:
+            resultado = this.dividir(num1, num2);
+            break;
+          default:
+            resultado = "Operación no válida";
+        }
+        alert("El resultado es: " + resultado);
+      }
+    } while (continuar);
+  }
+};
+
+calculadora.calcular();
